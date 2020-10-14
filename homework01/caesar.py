205049c38e0ee370dbb92998e48dd94117de828f
Dre4my26 <dreamyguy26@gmail.com>
2020-10-14 16:29:59 -0700

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -15,9 +15,16 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i in range (len(plaintext)):
+        if ord('a') <= ord(plaintext[i]) < ord('x') or ord('A') <= ord(plaintext[i]) < ord('X'):
+            new_letter = ord(plaintext[i]) + shift
+            ciphertext += chr(new_letter)
+        elif ord('x') <= ord(plaintext[i]) <= ord('z') or ord('X') <= ord(plaintext[i]) <= ord('Z'):
+            new_letter = ord(plaintext[i]) - 26 + shift
+            ciphertext += chr(new_letter)
+        else:
+            ciphertext += plaintext[i]
     return ciphertext
-
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
@@ -33,9 +40,17 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
-    return plaintext
+    for i in range (len(ciphertext)):
 
+      if ord('d') < ord(ciphertext[i]) < ord('z') or ord('D') <= ord(ciphertext[i]) < ord('Z'):
+        new_letter = ord(ciphertext[i]) - shift
+        plaintext += chr(new_letter)
+      elif ord('a') <= ord(ciphertext[i]) <= ord('c') or ord('A') <= ord(ciphertext[i]) <= ord('C'):
+        new_letter = ord(ciphertext[i]) - shift + 26
+        plaintext += chr(new_letter)
+      else:
+        plaintext += ciphertext[i]
+    return plaintext
 
 def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     """
