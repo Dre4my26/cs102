f212e59802d5416c146f39dd2c9395f0d6066183
Dre4my26 <dreamyguy26@gmail.com>
2020-10-16 13:12:52 -0700

    function decrypt_vegenere is implemented

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -11,21 +11,22 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     ciphertext = ""
     for i in range(len(plaintext)):
-      letter_new = ord(plaintext[i])
-      key = ord(keyword[i % len(keyword)])
-      if letter_new >= ord('A') and letter_new <= ord('Z'):
-        if key >= ord('A') and key <= ord('Z'):
-          ciphertext += chr((letter_new - ord('A') + key - ord('A')) % 26 + ord('A'))
+        letter_new = ord(plaintext[i])
+        key = ord(keyword[i % len(keyword)])
+        if letter_new >= ord("A") and letter_new <= ord("Z"):
+            if key >= ord("A") and key <= ord("Z"):
+                ciphertext += chr((letter_new - ord("A") + key - ord("A")) % 26 + ord("A"))
+            else:
+                ciphertext += chr((letter_new + key - ord("A") - ord("a")) % 26 + ord("A"))
+        elif letter_new >= ord("a") and letter_new <= ord("z"):
+            if key >= ord("A") and key <= ord("Z"):
+                ciphertext += chr((letter_new + key - ord("a") - ord("A")) % 26 + ord("a"))
+            else:
+                ciphertext += chr((letter_new - ord("a") + key - ord("a")) % 26 + ord("a"))
         else:
-          ciphertext += chr((letter_new + key - ord('A') - ord('a')) % 26 + ord('A'))
-      elif letter_new >= ord("a") and letter_new <= ord("z"):
-        if key>= ord('A') and key <= ord('Z'):
-          ciphertext += chr((letter_new + key - ord('a') - ord('A')) % 26 + ord('a'))
-        else:
-          ciphertext += chr((letter_new - ord('a') + key - ord('a')) % 26 + ord('a'))
-      else:
-        ciphertext += chr(letter_new)
+            ciphertext += chr(letter_new)
     return ciphertext
+
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
@@ -40,16 +41,16 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     plaintext = ""
     for i in range(len(ciphertext)):
-      letter_new = ord(ciphertext[i])
-      key = ord(keyword[i % len(keyword)])
-      if letter_new >= ord('A') and letter_new <= ord('Z'):
-        if key >= ord('A') and key <= ord('Z'):
-          plaintext += chr((letter_new - ord('A') - key + ord('A')) % 26 + ord('A'))
-        else:
-          plaintext += chr((letter_new - ord('A ') - key + ord('a')) % 26 + ord('A'))
-      elif letter_new >= ord('a') and letter_new <= ord('z'):
-        if key >= ord('A') and key <= ord('Z'):
-          plaintext += chr((letter_new - ord('a') - key + ord('A')) % 26 + ord('a'))
-        else:
-          plaintext += chr((letter_new - ord('a') - key + ord('a')) % 26 + ord('a'))
+        letter_new = ord(ciphertext[i])
+        key = ord(keyword[i % len(keyword)])
+        if letter_new >= ord("A") and letter_new <= ord("Z"):
+            if key >= ord("A") and key <= ord("Z"):
+                plaintext += chr((letter_new - ord("A") - key + ord("A")) % 26 + ord("A"))
+            else:
+                plaintext += chr((letter_new - ord("A ") - key + ord("a")) % 26 + ord("A"))
+        elif letter_new >= ord("a") and letter_new <= ord("z"):
+            if key >= ord("A") and key <= ord("Z"):
+                plaintext += chr((letter_new - ord("a") - key + ord("A")) % 26 + ord("a"))
+            else:
+                plaintext += chr((letter_new - ord("a") - key + ord("a")) % 26 + ord("a"))
     return plaintext
