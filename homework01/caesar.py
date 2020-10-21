695df1d37dc115ccdee92e7a5657d2c26e406cb3
Dre4my26 <dreamyguy26@gmail.com>
2020-10-21 05:20:05 -0700

    passed unittestspython -m unittest -v tests.test_caesar!

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,4 +1,5 @@
 import typing as tp
+import string
 
 
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
@@ -16,15 +17,16 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     ciphertext = ""
     for i in range(len(plaintext)):
-        if ord("a") <= ord(plaintext[i]) < ord("x") or ord("A") <= ord(plaintext[i]) < ord("X"):
-            new_letter = ord(plaintext[i]) + shift
-            ciphertext += chr(new_letter)
-        elif ord("x") <= ord(plaintext[i]) <= ord("z") or ord("X") <= ord(plaintext[i]) <= ord("Z"):
-            new_letter = ord(plaintext[i]) - 26 + shift
-            ciphertext += chr(new_letter)
-            if ord(ciphertext[i]) < ord('A') or ord('Z') < ord(ciphertext[i]) < ord('a'):
-              ciphertext = ciphertext[:-1]
-              ciphertext += plaintext[i]
+        if ord('A') <= ord(plaintext[i]) <= ord('Z'):
+            if ord('Z') - ord(plaintext[i]) < shift:
+                ciphertext += chr(ord('A') - 1 + (shift - ord('Z') + ord(plaintext[i])))
+            else:
+                ciphertext += chr(ord(plaintext[i]) + shift)
+        elif ord('a') <= ord(plaintext[i]) <= ord('z'):
+            if ord('z') - ord(plaintext[i]) < shift:
+                ciphertext += chr(ord('a') - 1 + (shift - ord('z') + ord(plaintext[i])))
+            else:
+                ciphertext += chr(ord(plaintext[i]) + shift)
         else:
             ciphertext += plaintext[i]
           
@@ -45,16 +47,21 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    for i in range(len(ciphertext)):
-
-        if ord("d") < ord(ciphertext[i]) < ord("z") or ord("D") <= ord(ciphertext[i]) < ord("Z"):
-            new_letter = ord(ciphertext[i]) - shift
-            plaintext += chr(new_letter)
-        elif ord("a") <= ord(ciphertext[i]) <= ord("c") or ord("A") <= ord(ciphertext[i]) <= ord("C"):
-            new_letter = ord(ciphertext[i]) - shift + 26
-            plaintext += chr(new_letter)
+    for i in range (len(ciphertext)):
+        if ord('A') <= ord(ciphertext[i]) <= ord('Z'):
+            if ord(ciphertext[i]) - shift < ord('A'):
+                plaintext += chr(ord('Z') + 1 - (shift - (ord(ciphertext[i]) - ord('A'))))
+            else:
+                 plaintext += chr(ord(ciphertext[i]) - shift)
+        
+        elif ord('a') <= ord(ciphertext[i]) <= ord('z'):
+            if ord(ciphertext[i]) - shift < ord('a'):
+                plaintext += chr(ord('z') + 1 - (shift-(ord(ciphertext[i]) - ord('a'))))
+            else:
+                 plaintext += chr(ord(ciphertext[i]) - shift)
         else:
             plaintext += ciphertext[i]
+
     return plaintext
 
 
