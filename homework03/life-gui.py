610e036fb287db54ac3eb134481c6da85d8f3caa
Dre4my26 <dreamyguy26@gmail.com>
2021-03-04 10:49:43 -0800

    Better late than never, I guess

--- homework03/life-gui.py
+++ homework03/life-gui.py
@@ -1,23 +1,107 @@
-import pygame
-from pygame.locals import *
+# pylint: disable=no-member
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+# pyling: disable=unused-wildcard-import
 
+import pygame  # type: ignore
+from pygame.locals import *  # type: ignore
+import life
 from life import GameOfLife
 from ui import UI
 
 
 class GUI(UI):
-
-    def __init__(self, life: GameOfLife, cell_size: int=10, speed: int=10) -> None:
+    def __init__(self, life: GameOfLife, cell_size: int = 10, speed: int = 10) -> None:
         super().__init__(life)
 
+        self.width = self.life.cols * cell_size
+        self.height = self.life.rows * cell_size
+        self.cell_size = cell_size
+
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.speed = speed
+
     def draw_lines(self) -> None:
-        # Copy from previous assignment
-        pass
+        for x_c in range(0, self.width, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color("black"), (x_c, 0), (x_c, self.height))
+
+        for y_c in range(0, self.height, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color("black"), (0, y_c), (self.width, y_c))
 
     def draw_grid(self) -> None:
-        # Copy from previous assignment
-        pass
+        for i in range(len(self.life.curr_generation)):
+            for j in range(len(self.life.curr_generation[i])):
+                cell_color = (
+                    pygame.Color("green")
+                    if self.life.curr_generation[i][j]
+                    else pygame.Color("white")
+                )
+                rect = pygame.Rect(
+                    j * self.cell_size,
+                    i * self.cell_size,
+                    self.cell_size,
+                    self.cell_size,
+                )
+                pygame.draw.rect(self.screen, cell_color, rect)
+
+    def change_state(self, cell: life.Cell) -> None:
+        cell_x = cell[0] // self.cell_size
+        cell_y = cell[1] // self.cell_size
+        if self.life.curr_generation[cell_x][cell_y]:
+            self.life.curr_generation[cell_x][cell_y] = 0
+        else:
+            self.life.curr_generation[cell_x][cell_y] = 1
 
     def run(self) -> None:
-        # Copy from previous assignment
-        pass
+        pygame.init()
+        clock = pygame.time.Clock()
+        pygame.display.set_caption("Game of Life")
+        self.screen.fill(pygame.Color("white"))
+
+        running = True
+        pause = False
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    (
+                        y_c,
+                        x_c,
+                    ) = pygame.mouse.get_pos()
+                    self.change_state((x_c, y_c))
+                    self.draw_grid()
+                    self.draw_lines()
+                    clock.tick(self.speed)
+                    pygame.display.flip()
+                    continue
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        if pause:
+                            pause = False
+                        else:
+                            pause = True
+            if pause:
+                continue
+
+            # Отрисовка списка клеток
+            self.draw_grid()
+            self.draw_lines()
+
+            # Выполнение одного шага игры (обновление состояния ячеек)
+            self.life.step()
+
+            clock.tick(self.speed)
+            pygame.display.flip()
+        pygame.quit()
+
+
+def main():
+    game = GameOfLife(size=(48, 64))
+    app = GUI(game)
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
--- homework03/life.py
+++ homework03/life.py
@@ -1,6 +1,8 @@
+import json
 import pathlib
 import random
-
+from copy import deepcopy
+from itertools import product
 from typing import List, Optional, Tuple
 
 
@@ -10,12 +12,11 @@ Grid = List[Cells]
 
 
 class GameOfLife:
-    
     def __init__(
         self,
         size: Tuple[int, int],
-        randomize: bool=True,
-        max_generations: Optional[float]=float('inf')
+        randomize: bool = True,
+        max_generations: Optional[float] = float("inf"),
     ) -> None:
         # Размер клеточного поля
         self.rows, self.cols = size
@@ -28,47 +29,105 @@ class GameOfLife:
         # Текущее число поколений
         self.generations = 1
 
-    def create_grid(self, randomize: bool=False) -> Grid:
-        # Copy from previous assignment
-        pass
+    def create_grid(self, randomize: bool = False) -> Grid:
+        return [
+            [random.randint(
+                0, 1) if randomize else 0 for _ in range(self.cols)]
+            for _ in range(self.rows)
+        ]
+
+    def _is_valid_cell(self, candidate: Cell) -> bool:
+        return 0 <= candidate[0] < self.rows and 0 <= candidate[1] < self.cols
 
     def get_neighbours(self, cell: Cell) -> Cells:
         # Copy from previous assignment
-        pass
+        neighbours = []
+
+        deltas = [-1, 0, 1]
+        for cord_dx, cord_dy in product(deltas, deltas):
+            if (cord_dx, cord_dy) == (0, 0):
+                continue
+
+            row, col = cell[0] + cord_dy, cell[1] + cord_dx
+            if self._is_valid_cell((row, col)):
+                is_alive = self.curr_generation[row][col]
+                neighbours.append(is_alive)
+
+        return neighbours
 
     def get_next_generation(self) -> Grid:
-        # Copy from previous assignment
-        pass
+        out = deepcopy(self.curr_generation)
+
+        for i in range(len(out)):
+            for j in range(len(out[i])):
+                alive_neighbours = sum(self.get_neighbours((i, j)))
+
+                if self.curr_generation[i][j]:
+                    out[i][j] = int(2 <= alive_neighbours <= 3)
+                elif alive_neighbours == 3:
+                    out[i][j] = 1
+
+        return out
 
     def step(self) -> None:
         """
         Выполнить один шаг игры.
         """
-        pass
+        self.prev_generation = deepcopy(self.curr_generation)
+        self.curr_generation = self.get_next_generation()
+        self.generations += 1
 
     @property
     def is_max_generations_exceeded(self) -> bool:
         """
         Не превысило ли текущее число поколений максимально допустимое.
         """
-        pass
+        return self.generations >= self.max_generations
 
     @property
     def is_changing(self) -> bool:
         """
         Изменилось ли состояние клеток с предыдущего шага.
         """
-        pass
+        return self.prev_generation != self.curr_generation
 
     @staticmethod
-    def from_file(filename: pathlib.Path) -> 'GameOfLife':
+    def from_file(filename: pathlib.Path) -> "GameOfLife":
         """
         Прочитать состояние клеток из указанного файла.
         """
-        pass
+        start_gen = []
+        with open(filename, "r") as game_file:
+            for line in game_file.readlines():
+                if not line == "\n":
+                    start_gen.append([int(i) for i in line if i in (0, 1)])
 
-    def save(filename: pathlib.Path) -> None:
-        """
-        Сохранить текущее состояние клеток в указанный файл.
-        """
-        pass
+        size = len(start_gen), len(start_gen[0])
+        game = GameOfLife(size=size, randomize=False)
+        game.curr_generation = start_gen
+        return game
+
+    def save(self, filename: pathlib.Path) -> None:
+        with open(filename, "w") as f:
+            json.dump(self.curr_generation, fp=f)
+
+
+def main():
+    filename = "generation_{}.txt"
+
+    def gen_path(step):
+        return pathlib.Path(filename.format(step)).resolve()
+
+    # New game
+    game = GameOfLife(size=(48, 64))
+    game.step()
+    game.save(gen_path(0))
+
+    # Game from save
+    game = GameOfLife.from_file(gen_path(0))
+    game.step()
+    game.save(gen_path(1))
+
+
+if __name__ == "__main__":
+    main()
