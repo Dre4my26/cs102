acd79e1331ef93b1e8d18964c47d4a44dad96e2d
Dre4my26 <dreamyguy26@gmail.com>
2021-04-22 01:53:57 -0700

    almost made scraputils

--- homework06/scraputils.py
+++ homework06/scraputils.py
@@ -5,16 +5,33 @@ from bs4 import BeautifulSoup
 def extract_news(parser):
     """ Extract news from a given web page """
     news_list = []
+    
+    texts = parser.find_all('a', class_='storylink')
+    for text in texts:
+        print(text.text)
 
-    # PUT YOUR CODE HERE
+    points = parser.find_all('span', class_='score')
+    for point in points:
+        print(point.text)
 
+    authors = parser.find_all('a', class_='hnuser')
+    for author in authors:
+        print(author.text)
+
+    links = parser.find_all('a', class_='storylink')
+    for link in links:
+        print(link.get('href'))
+    """
+    comments = parser.find_all('a')
+    print(comments)
+    """
     return news_list
 
 
 def extract_next_page(parser):
     """ Extract next page URL """
     # PUT YOUR CODE HERE
-
+    return ""
 
 def get_news(url, n_pages=1):
     """ Collect news from a given web page """
