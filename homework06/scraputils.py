e7665983ae9d0f48525a6688bc9875aad20fe69a
Dre4my26 <dreamyguy26@gmail.com>
2021-04-27 10:01:52 -0700

    works but badly

--- homework06/scraputils.py
+++ homework06/scraputils.py
@@ -5,33 +5,60 @@ from bs4 import BeautifulSoup
 def extract_news(parser):
     """ Extract news from a given web page """
     news_list = []
-    """
+    news_dict = {'author', 'comments', 'points', 'title', 'link'}
+    news_quantity = 0
+
     texts = parser.find_all('a', class_='storylink')
     for text in texts:
-        print(text.text)
+        news_list.append(text.text)
+        news_quantity += 1
+
 
     points = parser.find_all('span', class_='score')
     for point in points:
-        print(point.text)
+        points_text = point.text
+        points_splitted = points_text.split()
+        points_total = points_splitted[0]
+        news_list.append(points_total)
 
     authors = parser.find_all('a', class_='hnuser')
     for author in authors:
-        print(author.text)
+        news_list.append(author.text)
 
     links = parser.find_all('a', class_='storylink')
     for link in links:
-        print(link.get('href'))
-    """
-    comments = parser.findall('a', class_='href')
-    for comment in comments:
-        print(comment.text)
-    return news_list
+        news_list.append(link.get('href'))
 
+    comments_filtered = []
+    comments = parser.find_all(class_='subtext')[:-1]
+    for comment in comments:
+        last = comment.text
+        subtext_splitted = last.split()
+        if subtext_splitted[-1] == 'discuss':
+            comments_filtered.append(0)
+        elif subtext_splitted[-1] == ('comment' or 'comments'):
+            if not isinstance(subtext_splitted[-2:-1], int):
+                comments_filtered.append(int(subtext_splitted[-2:-1][0]))
+    for comment_points in comments_filtered:
+        if isinstance(comment_points, int):
+            news_list.append(int(comments_filtered[comment_points]))
+
+    """
+    for news in range(news_quantity):
+        news_dict['title'] = news_list[news]
+        news_dict['points'] = news_list[news_quantity + news]
+        news_dict['author'] = news_list[2*news_quantity + news]
+        news_dict['link'] = news_list[3*news_quantity + news]
+        news_dict['comments'] = news_list[4*news_quantity + news]
+    print(news_dict)
+    """
+    return news_list
 
 def extract_next_page(parser):
     """ Extract next page URL """
-    # PUT YOUR CODE HERE
-    return ""
+    next_page_link = parser.find_all('a', class_='morelink')
+    next_page_link = next_page_link[0].get('href')
+    return next_page_link
 
 
 def get_news(url, n_pages=1):
